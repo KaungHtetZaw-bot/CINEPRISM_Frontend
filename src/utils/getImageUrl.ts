c995441610f823fafae1cb4064053367e9ba3a74
kaung htet zaw <zawk37010@gmail.com>
2026-03-01 15:16:24 +0630

    add default image

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,6 +1,7 @@
 const TMDB_BASE_URL = "https://image.tmdb.org/t/p/";
+import defaultPoster from '../assets/images/default-poster.png'
 
 export const getImageUrl = (path: string | null, size: 'w200' | 'w500' | 'w780' | 'w1280' | 'original' = "w500") => {
-  if (!path) return "/placeholder-poster.png";
+  if (!path) return defaultPoster;
   return `${TMDB_BASE_URL}${size}${path}`;
 };
